85f7819df4e3d319c1e4f2e84ae25b567599fe2a
giorgi <gd@giorgis-MacBook-Pro.local>
2020-12-30 10:59:31 +0400

    updating ouar app to redirect to home if user is signed in

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,4 +1,4 @@
-import { setCurrentUser } from './userAction'
+import { UserActionTypes } from './userActionTypes'
 
 const INITIAL_STATE = {
   currentUser: null,
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case setCurrentUser.SET_CURRENT_USER:
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
